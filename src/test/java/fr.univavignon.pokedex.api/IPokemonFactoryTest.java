9ce9b7d5854d2ef5ab8a7207729a1f41fda0ba9e
calvintomqwe <tom.calvin.aze@gmail.com>
2024-12-19 03:21:39 +0100

    updated tests

--- src/test/java/fr.univavignon.pokedex.api/IPokemonFactoryTest.java
+++ src/test/java/fr.univavignon.pokedex.api/IPokemonFactoryTest.java
@@ -3,6 +3,7 @@ package fr.univavignon.pokedex.api;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,6 +12,7 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class IPokemonFactoryTest {
 
+    @Mock
     private IPokemonFactory pokemonFactory;
 
     @BeforeEach
