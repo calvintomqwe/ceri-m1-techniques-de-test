41981016e45200b10380e15e90dafb1ccc6edb43
calvintomqwe <tom.calvin.aze@gmail.com>
2024-12-19 04:25:30 +0100

    updated tests

--- src/test/java/fr.univavignon.pokedex.api/IPokedexTest.java
+++ src/test/java/fr.univavignon.pokedex.api/IPokedexTest.java
@@ -11,31 +11,35 @@ public class IPokedexTest {
     private IPokedex pokedex;
     private Pokemon bulbizarre;
     private Pokemon aquali;
+    private Pokemon pikachu;
     private Comparator<Pokemon> byName;
 
     @Before
     public void setUp() throws PokedexException {
+
         PokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
         PokemonFactory pokemonFactory = new PokemonFactory();
         pokedex = new Pokedex(metadataProvider, pokemonFactory);
 
         bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56.0);
         aquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100.0);
+        pikachu = new Pokemon(25, "Pikachu", 112, 90, 120, 3200, 150, 3500, 3, 35.0);
 
         pokedex.addPokemon(bulbizarre);
+        pokedex.addPokemon(aquali);
 
         byName = Comparator.comparing(Pokemon::getName);
+
     }
 
     @Test
     public void testSize() {
-        int size = pokedex.size();
-        assertEquals(2, size);
+        assertEquals(2, pokedex.size());
     }
 
     @Test
     public void testAddPokemon() {
-        assertEquals(2, pokedex.addPokemon(bulbizarre));
+        assertEquals(2, pokedex.addPokemon(pikachu));
         assertEquals(3, pokedex.size());
     }
 
@@ -75,11 +79,11 @@ public class IPokedexTest {
         Pokemon newPokemon = pokedex.createPokemon(25, 150, 120, 2000, 3);
 
         assertNotNull(newPokemon);
-        assertEquals(25, newPokemon.getIndex());
-        assertEquals(150, newPokemon.getCp());
-        assertEquals(120, newPokemon.getHp());
-        assertEquals(2000, newPokemon.getDust());
-        assertEquals(3, newPokemon.getCandy());
+        assertEquals(25, newPokemon.getIndex());  // Vérifie l'index
+        assertEquals(150, newPokemon.getCp());  // Vérifie le CP
+        assertEquals(120, newPokemon.getHp());  // Vérifie les HP
+        assertEquals(2000, newPokemon.getDust());  // Vérifie le nombre de poussière
+        assertEquals(3, newPokemon.getCandy());  // Vérifie le nombre de bonbons
     }
 
     @Test
@@ -87,12 +91,11 @@ public class IPokedexTest {
         PokemonMetadata metadata = pokedex.getPokemonMetadata(0);
 
         assertNotNull(metadata);
-        assertEquals("Bulbizarre", metadata.getName());
+        assertEquals("Bulbizarre", metadata.getName());  // Vérifie que le nom est correct pour l'index 0
     }
 
     @Test
     public void testGetPokemonInvalidIdNegative() {
-
         PokedexException exception = assertThrows(PokedexException.class, () -> {
             pokedex.getPokemon(-1);
         });
@@ -102,7 +105,6 @@ public class IPokedexTest {
 
     @Test
     public void testGetPokemonInvalidIdTooHigh() {
-
         PokedexException exception = assertThrows(PokedexException.class, () -> {
             pokedex.getPokemon(999);  // Supposons qu'il n'y ait pas 999 Pokémon
         });
@@ -117,12 +119,13 @@ public class IPokedexTest {
         IPokedex anotherPokedex = new Pokedex(metadataProvider, pokemonFactory);
 
         anotherPokedex.addPokemon(bulbizarre);
-
-        assertEquals(pokedex, anotherPokedex);
-
         anotherPokedex.addPokemon(aquali);
 
-        assertNotEquals(pokedex, anotherPokedex);
+        assertEquals(pokedex, anotherPokedex);  // Les deux pokedex doivent être égaux
+
+        anotherPokedex.addPokemon(pikachu);
+
+        assertNotEquals(pokedex, anotherPokedex);  // Ils ne doivent plus être égaux après l'ajout d'un Pokémon
     }
 
     @Test
@@ -132,35 +135,36 @@ public class IPokedexTest {
         IPokedex anotherPokedex = new Pokedex(metadataProvider, pokemonFactory);
 
         anotherPokedex.addPokemon(bulbizarre);
-
-        assertEquals(pokedex.hashCode(), anotherPokedex.hashCode());
-
         anotherPokedex.addPokemon(aquali);
 
-        assertNotEquals(pokedex.hashCode(), anotherPokedex.hashCode());
+        assertEquals(pokedex.hashCode(), anotherPokedex.hashCode());  // Les hashCode doivent être égaux
+
+        anotherPokedex.addPokemon(pikachu);
+
+        assertNotEquals(pokedex.hashCode(), anotherPokedex.hashCode());  // Les hashCode doivent être différents après l'ajout d'un Pokémon
     }
 
     @Test
     public void testEqualsWithSelf() {
-        assertEquals(pokedex, pokedex);
+        assertEquals(pokedex, pokedex);  // Un objet est toujours égal à lui-même
     }
 
     @Test
     public void testEqualsWithNull() {
-        assertNotEquals(pokedex, null);
+        assertNotEquals(pokedex, null);  // null ne doit pas être égal au Pokedex
     }
 
 
     @Test
     public void testEqualsWithDifferentClass() {
-        Object notPokedex = new Object();
-        assertNotEquals(pokedex, notPokedex);
+        Object notPokedex = new Object();  // Créer un objet d'une classe différente
+        assertNotEquals(pokedex, notPokedex);  // Le Pokedex ne doit pas être égal à un objet d'une classe différente
     }
 
     @Test
     public void testEqualsWithDifferentObjectType() {
-        Object notPokedex = new Object();
-        assertNotEquals(pokedex, notPokedex);
+        Object notPokedex = new Object();  // Créer un objet d'une classe différente
+        assertNotEquals(pokedex, notPokedex);  // Le Pokedex ne doit pas être égal à un objet d'une classe différente
     }
 
 
