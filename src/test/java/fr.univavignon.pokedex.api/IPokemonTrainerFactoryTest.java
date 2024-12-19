a1ad730764b8d5cff69f12e418d9a6ed75fef057
calvintomqwe <tom.calvin.aze@gmail.com>
2024-12-19 01:06:17 +0100

    updated tests

--- src/test/java/fr.univavignon.pokedex.api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr.univavignon.pokedex.api/IPokemonTrainerFactoryTest.java
@@ -6,6 +6,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(MockitoExtension.class)
 public class IPokemonTrainerFactoryTest {
 
@@ -27,5 +29,47 @@ public class IPokemonTrainerFactoryTest {
         trainerFactory.createTrainer("Ash", Team.MYSTIC, pokedexFactory);
 
         Mockito.verify(trainerFactory).createTrainer("Ash", Team.MYSTIC, pokedexFactory);
+    }
+
+    @Test
+    public void testGetName(){
+        PokemonTrainer trainer = Mockito.mock(PokemonTrainer.class);
+
+        Mockito.when(trainer.getName()).thenReturn("Ash");
+
+        String name = trainer.getName();
+
+        Mockito.verify(trainer).getName();
+
+        assertEquals("Ash", name);
+    }
+
+    @Test
+    public void testGetTeam(){
+        PokemonTrainer trainer = Mockito.mock(PokemonTrainer.class);
+
+        Mockito.when(trainer.getTeam()).thenReturn(Team.MYSTIC);
+
+        Team team = trainer.getTeam();
+
+        Mockito.verify(trainer).getTeam();
+
+        assertEquals(Team.MYSTIC, team);
+    }
+
+    @Test
+    public void testGetPokedex(){
+
+        PokemonTrainer trainer = Mockito.mock(PokemonTrainer.class);
+
+        IPokedex mockedPokedex = Mockito.mock(IPokedex.class);
+
+        Mockito.when(trainer.getPokedex()).thenReturn(mockedPokedex);
+
+        IPokedex pokedex = trainer.getPokedex();
+
+        Mockito.verify(trainer).getPokedex();
+
+        assertEquals(mockedPokedex, pokedex);
     }
 }
