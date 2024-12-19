d60dcdb279e8e457b88660296654b87ba314cf96
calvintomqwe <tom.calvin.aze@gmail.com>
2024-12-19 01:55:49 +0100

    updated tests

--- src/test/java/fr.univavignon.pokedex.api/IPokemonFactoryTest.java
+++ src/test/java/fr.univavignon.pokedex.api/IPokemonFactoryTest.java
@@ -3,10 +3,10 @@ package fr.univavignon.pokedex.api;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class IPokemonFactoryTest {
@@ -16,82 +16,34 @@ public class IPokemonFactoryTest {
 
     @Test
     public void testCreatePokemon() {
-        Pokemon bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56.0);
+        // Example data for a Pokemon (let's say Bulbizarre)
+        int index = 0;  // Bulbizarre index
+        String name = "Bulbizarre";
+        int attack = 126;
+        int defense = 126;
+        int stamina = 90;
+        int cp = 613;
+        int hp = 64;
+        int dust = 4000;
+        int candy = 4;
+        double iv = 0.56;
 
-        Mockito.when(pokemonFactory.createPokemon(0, 613, 64, 4000, 4)).thenReturn(bulbizarre);
+        Pokemon expectedPokemon = new Pokemon(index, name, attack, defense, stamina, cp, hp, dust, candy, iv);
 
-        pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
+        when(pokemonFactory.createPokemon(index, cp, hp, dust, candy))
+                .thenReturn(expectedPokemon);
 
-        Mockito.verify(pokemonFactory).createPokemon(0, 613, 64, 4000, 4);
-    }
+        Pokemon result = pokemonFactory.createPokemon(index, cp, hp, dust, candy);
 
-    @Test
-    public void testIndex() {
-        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 40, 4, 56);
-        int index = pokemon.getIndex();
-        assertEquals(0, index);
-    }
-
-    @Test
-    public void testName() {
-        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 40, 4, 56);
-        String name = pokemon.getName();
-        assertEquals("Bulbizarre", name);
-    }
-
-    @Test
-    public void testAttack() {
-        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 40, 4, 56);
-        int attack = pokemon.getAttack();
-        assertEquals(126, attack);
-    }
-
-    @Test
-    public void testDefense() {
-        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 40, 4, 56);
-        int defense = pokemon.getDefense();
-        assertEquals(126, defense);
-    }
-
-    @Test
-    public void testStamina() {
-        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 40, 4, 56);
-        int stamina = pokemon.getStamina();
-        assertEquals(90, stamina);
-    }
-
-    @Test
-    public void testCp() {
-        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 40, 4, 56);
-        int cp = pokemon.getCp();
-        assertEquals(613, cp);
-    }
-
-    @Test
-    public void testHp() {
-        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 40, 4, 56);
-        int hp = pokemon.getHp();
-        assertEquals(64, hp);
-    }
-
-    @Test
-    public void testDust() {
-        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 40, 4, 56);
-        int dust = pokemon.getDust();
-        assertEquals(40, dust);
-    }
-
-    @Test
-    public void testCandy() {
-        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 40, 4, 56);
-        int candy = pokemon.getCandy();
-        assertEquals(4, candy);
-    }
-
-    @Test
-    public void testIv() {
-        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 40, 4, 56);
-        double iv = pokemon.getIv();
-        assertEquals(56, iv);
+        assertEquals(expectedPokemon.getIndex(), result.getIndex());
+        assertEquals(expectedPokemon.getName(), result.getName());
+        assertEquals(expectedPokemon.getAttack(), result.getAttack());
+        assertEquals(expectedPokemon.getDefense(), result.getDefense());
+        assertEquals(expectedPokemon.getStamina(), result.getStamina());
+        assertEquals(expectedPokemon.getCp(), result.getCp());
+        assertEquals(expectedPokemon.getHp(), result.getHp());
+        assertEquals(expectedPokemon.getDust(), result.getDust());
+        assertEquals(expectedPokemon.getCandy(), result.getCandy());
+        assertEquals(expectedPokemon.getIv(), result.getIv());
     }
 }
